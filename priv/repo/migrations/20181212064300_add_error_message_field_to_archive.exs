b91ebef9227581c8d4fe2f4547720102a64a1400
Azhar Malik <azharmalik02@gmail.com>
2018-12-13 11:13:03 +0500

    Added coding to save clip failure message

---
+++ priv/repo/migrations/20181212064300_add_error_message_field_to_archive.exs
@@ -0,0 +1,9 @@
+defmodule EvercamMedia.Repo.Migrations.AddErrorMessageFieldToArchive do
+  use Ecto.Migration
+
+  def change do
+    alter table(:archives) do
+      add :error_message, :text
+    end
+  end
+end
