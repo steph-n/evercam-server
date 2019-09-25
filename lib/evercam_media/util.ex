5b3dfb6d04743952fd2daa63fe82bb93fb889b6e
Azhar Malik <azharmalik02@gmail.com>
2019-09-25 15:20:06 +0500

    Reset snapshot request flag from outside the function

--- lib/evercam_media/util.ex
+++ lib/evercam_media/util.ex
@@ -348,7 +348,7 @@ defmodule EvercamMedia.Util do
     end
   end
 
-  # def camera_use_synchronous_req(camera_exid) when camera_exid in ["angel-ibvua", "everc-fhlcr", "everc-dupzo", "colog-uyceb", "colog-ywtlq", "waxie-jolxd"], do: true
+  # def camera_use_synchronous_req(camera_exid) when camera_exid in ["extra-uhjfn"], do: false
   def camera_use_synchronous_req(_), do: true
 
   def load_storage_servers([]) do
