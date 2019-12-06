8ca5e284374c1f3c6c3a089ea267b1a97a7ab6b4
javier <javier@evercam.io>
2019-12-06 13:11:53 +0000

    Return token on user confirm

--- lib/evercam_media_web/controllers/user_controller.ex
+++ lib/evercam_media_web/controllers/user_controller.ex
@@ -507,15 +507,24 @@ defmodule EvercamMediaWeb.UserController do
       |> Base.encode16
       |> String.downcase
     current_date = Calendar.DateTime.now_utc
+    exp =
+      Calendar.DateTime.now_utc
+      |> Calendar.DateTime.advance!(60 * 60 * 24 * 7)
     case (params["c"] == code) do
       true ->
         user_params = %{confirmed_at: current_date}
         changeset = User.changeset(user, user_params)
         case Repo.update(changeset) do
           {:ok, updated_user} ->
-            conn
-            |> put_status(201)
-            |> json(%{api_id: updated_user.api_id, api_key: updated_user.api_key})
+            extra_claims = %{
+              "user_id" => params["username"],
+              "exp" => exp |> DateTime.to_unix
+            }
+            with :ok <- ensure_user_exists(updated_user, username, conn),
+                {:ok, token, _} <- JwtAuthToken.generate_and_sign(extra_claims)
+            do
+              save_session(conn, token, updated_user, params)
+            end
           {:error, changeset} ->
             render_error(conn, 400, Util.parse_changeset(changeset))
         end
