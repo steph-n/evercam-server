9bf1a348d902ab544c39992e33b4bd8c47db5402
Junaid Farooq <junaid@evercam.io>
2020-01-14 11:40:30 +0500

    Added Media Url & Code Refactor.

--- lib/evercam_media_web/views/compare_view.ex
+++ lib/evercam_media_web/views/compare_view.ex
@@ -11,14 +11,14 @@ defmodule EvercamMediaWeb.CompareView do
     %{compares: render_many([compare], __MODULE__, "compare.#{version}.json")}
   end
 
-  def render("compare.v1.json", %{compare: compare}) do
+  def render("compare." <> <<version::binary-size(2)>> <> ".json", %{compare: compare}) do
     %{
       id: compare.exid,
       camera_id: compare.camera.exid,
       title: compare.name,
-      from_date: Util.ecto_datetime_to_unix(compare.before_date),
-      to_date: Util.ecto_datetime_to_unix(compare.after_date),
-      created_at: Util.ecto_datetime_to_unix(compare.inserted_at),
+      from_date: date_wrt_version(version, compare.before_date, compare.camera),
+      to_date: date_wrt_version(version, compare.after_date, compare.camera),
+      created_at: date_wrt_version(version, compare.inserted_at, compare.camera),
       status: status(compare.status),
       requested_by: Util.deep_get(compare, [:user, :username], ""),
       requester_name: User.get_fullname(compare.user),
@@ -28,34 +28,17 @@ defmodule EvercamMediaWeb.CompareView do
       frames: 2,
       public: compare.public,
       file_name: "",
-      media_url: "",
+      media_urls: %{
+        mp4: "#{EvercamMediaWeb.Endpoint.static_url}/#{version}/cameras/#{compare.camera.exid}/compares/#{compare.exid}.mp4",
+        gif: "#{EvercamMediaWeb.Endpoint.static_url}/#{version}/cameras/#{compare.camera.exid}/compares/#{compare.exid}.gif"
+      },
       type: "compare",
-      thumbnail_url: "#{EvercamMediaWeb.Endpoint.static_url}/v1/cameras/#{compare.camera.exid}/archives/#{compare.exid}/thumbnail?type=compare"
+      thumbnail_url: "#{EvercamMediaWeb.Endpoint.static_url}/#{version}/cameras/#{compare.camera.exid}/archives/#{compare.exid}/thumbnail?type=compare"
     }
   end
 
-  def render("compare.v2.json", %{compare: compare}) do
-    %{
-      id: compare.exid,
-      camera_id: compare.camera.exid,
-      title: compare.name,
-      from_date: Util.datetime_to_iso8601(compare.before_date, Snapmail.get_timezone(compare.camera)),
-      to_date: Util.datetime_to_iso8601(compare.after_date, Snapmail.get_timezone(compare.camera)),
-      created_at: Util.datetime_to_iso8601(compare.inserted_at, Snapmail.get_timezone(compare.camera)),
-      status: status(compare.status),
-      requested_by: Util.deep_get(compare, [:user, :username], ""),
-      requester_name: User.get_fullname(compare.user),
-      requester_email: Util.deep_get(compare, [:user, :email], ""),
-      embed_code: compare.embed_code,
-      embed_time: false,
-      frames: 2,
-      public: compare.public,
-      file_name: "",
-      media_url: "",
-      type: "compare",
-      thumbnail_url: "#{EvercamMediaWeb.Endpoint.static_url}/v1/cameras/#{compare.camera.exid}/archives/#{compare.exid}/thumbnail?type=compare"
-    }
-  end
+  defp date_wrt_version("v2", date, camera), do: Util.datetime_to_iso8601(date, Camera.get_timezone(camera))
+  defp date_wrt_version("v1", date, _camera), do: Util.ecto_datetime_to_unix(date)
 
   defp status(0), do: "Processing"
   defp status(1), do: "Completed"
